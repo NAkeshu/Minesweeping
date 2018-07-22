f44e315f275cff155891580660610d3f19c226d0
NAkeshu <watashinomail@163.com>
2018-07-22 16:17:13 +0800

    计算出炸弹周围的数字，并在棋盘的相应位置绘制出来

--- minesweeping.py
+++ minesweeping.py
@@ -23,49 +23,56 @@ def initMap(rootframe):
     """
     map = Canvas(rootframe, width=640, height=690, bg="white")
     map.create_rectangle(20, 70, 620, 670) # 扫雷的棋盘
-    for i in range(0, 12):
+    for i in range(0, MAP_WIDTH):
         map.create_line(20, 70+i*50, 620, 70+i*50) # 横线
         map.create_line(20+i*50, 70, 20+i*50, 670) # 竖线
     map.pack(side=BOTTOM)
     return map
 
-def setMessageText(canvas, text):
+def setMessageText(canvas):
     """
     计步（点击次数）或者计时
     :param frame: 游戏窗口
     :param text: 标题（计步或者计时）
     :return:
     """
-    canvas.create_text(510, 30, text=text)
+    canvas.create_text(510, 30, text="步数：")
 
-def drawDigitsOnSide(canvas, n):
+def drawDigitsOnSide(canvas):
     """
     标出棋盘边上的数字
     :param canvas:
     :return:
     """
-    for i in range(0, n):
+    for i in range(0, MAP_WIDTH):
         canvas.create_text(10, 95+i*50, text=str(i))
         canvas.create_text(45+i*50, 60, text=str(i))
 
-def drawDigitsAroundBomb(map):
+def initGameData(n):
     """
-    在扫雷的棋盘上标出地雷周围的数字
-    :param map: 棋盘画布
+    将nxn的游戏棋盘中每个小方格一次按照0～nxn-1的编号排好，
+    使得二维的棋盘数据可以通过一维的字典数据结构保存。
+    字典的键为方格编号，值为该方格中的数据。
+    其中，0代表空，>0代表炸弹数，-1代表有炸弹。
+    :param n:
     :return:
     """
-    pass
+    GameData = {}
+    for i in range(0, n*n):
+        GameData[str(i)] = 0
+    return GameData
 
-def initBombs(n):
+def initBombs(gameData):
     """
-    随机生成n个不同位置的编号，位置按照格子从左到右，从上到下的顺序，一共有12x12个
+    随机生成n个不同位置的编号，在数据里
     :param n: 生成炸弹数
     :return: 炸弹所在的格子编号的列表
     """
-    BombNum = random.sample(range(1, 12*12+1), n)
-    return BombNum
+    BombNum = random.sample(range(0, MAP_WIDTH*MAP_WIDTH), BOMBS_NUM)
+    for i in BombNum:
+        gameData[str(i)] = -1
 
-def drawBombs(mapcanvas, BombNum):
+def drawBombs(mapcanvas, gameData):
     """
     在扫雷画布上绘制出炸弹
     :param mapcanvas: 棋盘画布
@@ -74,22 +81,116 @@ def drawBombs(mapcanvas, BombNum):
     """
     global BombImg
     BombImg = PhotoImage(file="./img/BombMini.png")
-    n = len(BombNum)
+    n = len(gameData)
     for i in range(0, n):
-        x = 20 + 50*(BombNum[i] % 12)
-        y = 70 + 50*int(BombNum[i] / 12)
-        print("(" + str(x) + ", " + str(y) + ")")
-        mapcanvas.create_image(x, y, anchor='nw', image=BombImg)
+        if gameData[str(i)] == -1:
+            x = 20 + 50*(i % MAP_WIDTH)
+            y = 70 + 50*int(i / MAP_WIDTH)
+            print("(" + str(x) + ", " + str(y) + ")")
+            mapcanvas.create_image(x, y, anchor='nw', image=BombImg)
 
-def main():
+def drawDigitsAroundBomb(mapcanvas, gameData):
+    """
+    在扫雷的棋盘上标出地雷周围的数字
+    :param map: 棋盘画布
+    :return:
+    """
+    box = MAP_WIDTH*MAP_WIDTH
+    for i in range(0, box):
+        if gameData[str(i)] > 0:
+            x = 45 + 50*(i % MAP_WIDTH)
+            y = 95 + 50*int(i / MAP_WIDTH)
+            mapcanvas.create_text(x, y, text=str(gameData[str(i)]))
+
+def countAllDigits(gameData):
+    box = MAP_WIDTH*MAP_WIDTH
+    for i in range(0, box):
+        if gameData[str(i)] == -1:
+            countDigitAroundTheBomb(i, gameData)
+
+def countDigitAroundTheBomb(bombAddNum, gameData):
+    if bombAddNum > (MAP_WIDTH - 1):
+        if bombAddNum < (MAP_WIDTH*(MAP_WIDTH - 1) + 1):
+            countDigitOverBomb(bombAddNum, gameData)
+            countDigitBesideBomb(bombAddNum, gameData)
+            countDigitUnderBomb(bombAddNum, gameData)
+        else:
+            countDigitOverBomb(bombAddNum, gameData)
+            countDigitBesideBomb(bombAddNum, gameData)
+    else:
+        countDigitBesideBomb(bombAddNum, gameData)
+        countDigitUnderBomb(bombAddNum, gameData)
+
+def countDigitOverBomb(bombAddNum, gameData):
+    if bombAddNum % MAP_WIDTH > 0:
+        if bombAddNum % MAP_WIDTH < (MAP_WIDTH - 1):
+            for i in range(-1, 2):
+                if gameData[str(bombAddNum - MAP_WIDTH + i)] >= 0:
+                    gameData[str(bombAddNum - MAP_WIDTH + i)] += 1
+        else:
+            for i in range(-1, 1):
+                if gameData[str(bombAddNum - MAP_WIDTH + i)] >= 0:
+                    gameData[str(bombAddNum - MAP_WIDTH + i)] += 1
+    else:
+        for i in range(0, 2):
+            if gameData[str(bombAddNum - MAP_WIDTH + i)] >= 0:
+                gameData[str(bombAddNum - MAP_WIDTH + i)] += 1
+
+def countDigitBesideBomb(bombAddNum, gameData):
+    if bombAddNum % MAP_WIDTH > 0:
+        if bombAddNum % MAP_WIDTH < (MAP_WIDTH - 1):
+            for i in [-1, 1]:
+                if gameData[str(bombAddNum + i)] >= 0:
+                    gameData[str(bombAddNum + i)] += 1
+        else:
+            if gameData[str(bombAddNum - 1)] >= 0:
+                gameData[str(bombAddNum - 1)] += 1
+    else:
+        if gameData[str(bombAddNum + 1)] >= 0:
+            gameData[str(bombAddNum + 1)] += 1
+
+def countDigitUnderBomb(bombAddNum, gameData):
+    if bombAddNum % MAP_WIDTH > 0:
+        if bombAddNum % MAP_WIDTH < (MAP_WIDTH - 1):
+            for i in range(-1, 2):
+                if gameData[str(bombAddNum + MAP_WIDTH + i)] >= 0:
+                    gameData[str(bombAddNum + MAP_WIDTH + i)] += 1
+        else:
+            for i in range(-1, 1):
+                if gameData[str(bombAddNum + MAP_WIDTH + i)] >= 0:
+                    gameData[str(bombAddNum + MAP_WIDTH + i)] += 1
+    else:
+        for i in range(0, 2):
+            if gameData[str(bombAddNum + MAP_WIDTH + i)] >= 0:
+                gameData[str(bombAddNum + MAP_WIDTH + i)] += 1
+
+def initButton(mapcanvas, n):
+    """
+    在方格上放置按钮
+    :param mapcanvas:
+    :param n:
+    :return:
+    """
+    pass
+
+def startGame(map_width, bombs_num):
+    global MAP_WIDTH
+    global BOMBS_NUM
+    MAP_WIDTH = map_width
+    BOMBS_NUM = bombs_num
+    # MAP_WIDTH = 12
+    # BOMBS_NUM = 12
+
     root = initGameFrame("扫雷雷", 640, 690)
     gameMap = initMap(root)
-    drawDigitsOnSide(gameMap, 12)
-    setMessageText(gameMap, "步数：")
-    Bombs = initBombs(12)
-    drawBombs(gameMap, Bombs)
-    #drawDigitsAndBombs(gameMap)
+    drawDigitsOnSide(gameMap)
+    setMessageText(gameMap)
+    gameData = initGameData(MAP_WIDTH)
+    initBombs(gameData)
+    drawBombs(gameMap, gameData)
+    countAllDigits(gameData)
+    drawDigitsAroundBomb(gameMap, gameData)
     root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    startGame(12, 24)
