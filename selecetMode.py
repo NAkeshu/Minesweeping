281a31b1e16d2b85e80f00eadb427fc6414cfaf1
NAkeshu <watashinomail@163.com>
2018-07-25 21:01:58 +0800

    优化代码，调节游戏难度

--- selecetMode.py
+++ selecetMode.py
@@ -1,52 +1,44 @@
 from tkinter import *
 import minesweeping
 
-def selectEasyMode():
-    global MAP_WIDTH
-    global BOMBS_NUM
-    MAP_WIDTH = 6
-    BOMBS_NUM = 3
+class selectMode():
+    def __init__(self):
+        self.MAP_WIDTH = 10
+        self.BOMBS_NUM = 10
+        self.frame = Tk()
+        self.frame.wm_title("选择难度")
+        self.frame.minsize(300, 200)
+        selectLabelFrame = LabelFrame(self.frame, text="请选择难度：")
+        selectLabelFrame.pack(expand=YES)
+        easyRadio = Radiobutton(selectLabelFrame, text="简单难度（10x10,10个地雷）", command=self.selectEasyMode)
+        normalRadio = Radiobutton(selectLabelFrame, text="中等难度（15x15，60个地雷）", command=self.selectNormalMode)
+        hardRadio = Radiobutton(selectLabelFrame, text="困难难度（20x20,175个地雷）", command=self.selectHardMode)
+        determineButton = Button(selectLabelFrame, text="确认", command=self.determineMode)
+        easyRadio.pack(anchor=W)
+        normalRadio.pack(anchor=W)
+        hardRadio.pack(anchor=W)
+        determineButton.pack()
+        self.textlabel = Label(self.frame, text="")
+        self.textlabel.pack()
+        self.frame.mainloop()
 
-    textlabel['text'] = "已选择：简单难度"
+    def selectEasyMode(self):
+        self.MAP_WIDTH = 10
+        self.BOMBS_NUM = 10
+        self.textlabel['text'] = "已选择：简单难度"
 
-def selectNormalMode():
-    global MAP_WIDTH
-    global BOMBS_NUM
-    MAP_WIDTH = 12
-    BOMBS_NUM = 24
+    def selectNormalMode(self):
+        self.MAP_WIDTH = 15
+        self.BOMBS_NUM = 60
+        self.textlabel['text'] = "已选择：中等难度"
 
-    textlabel['text'] = "已选择：中等难度"
+    def selectHardMode(self):
+        self.MAP_WIDTH = 20
+        self.BOMBS_NUM = 175
+        self.textlabel['text'] = "已选择：困难难度"
 
-def selectHardMode():
-    global MAP_WIDTH
-    global BOMBS_NUM
-    MAP_WIDTH = 15
-    BOMBS_NUM = 75
-
-    textlabel['text'] = "已选择：困难难度"
-
-def determineMode():
-    minesweeping.startGame(MAP_WIDTH, BOMBS_NUM)
-
-
-def showModes():
-    global textlabel
-    frame = Tk()
-    frame.wm_title("选择难度")
-    frame.minsize(300, 200)
-    selectLabelFrame = LabelFrame(frame, text="请选择难度：")
-    selectLabelFrame.pack(expand=YES)
-    easyRadio = Radiobutton(selectLabelFrame, text="简单难度（6x6,3个地雷）", command=selectEasyMode)
-    normalRadio = Radiobutton(selectLabelFrame, text="中等难度（12x12，24个地雷）", command=selectNormalMode)
-    hardRadio = Radiobutton(selectLabelFrame, text="困难难度（15x15,75个地雷）", command=selectHardMode)
-    determineButton = Button(selectLabelFrame, text="确认", command=determineMode)
-    easyRadio.pack(anchor=W)
-    normalRadio.pack(anchor=W)
-    hardRadio.pack(anchor=W)
-    determineButton.pack()
-    textlabel = Label(frame, text="")
-    textlabel.pack()
-    frame.mainloop()
+    def determineMode(self):
+        minesweeping.minsweeping(self.MAP_WIDTH, self.BOMBS_NUM)
 
 if __name__ == "__main__":
-    showModes()
+    selectMode()
